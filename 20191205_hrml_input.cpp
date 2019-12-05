6244d6f3b588540c59bf62cea513f50938715231
Idnika <sizna35@gmail.com>
2019-12-05 10:47:29 +0900

    Add files via upload

--- 20191205_hrml_input.cpp
+++ 20191205_hrml_input.cpp
@@ -54,6 +54,7 @@ int main() { // main
     dtree* ins2;
     damn* insd;
     bool closetag = false;
+    string asdf;
     
     vector<damn> v;
 
@@ -62,8 +63,6 @@ int main() { // main
 
     for (int i = 0; i < a; i++) { // firstfor
         cin >> str;
-        cout << str << endl;
-        cout << str[1] << endl;
 
         if (str[1] == '/') { // 닫는 태그일 때
             str = str.substr(2, str.size() - 3);
@@ -77,19 +76,29 @@ int main() { // main
                 cout << "Now is " << now->num_value << endl;
             }
 
-            now = now->before;
+            // now = now->before;
 
-            if (now != NULL)
-            cout << "now's before: " << now->num_value << endl;
+            if (now != NULL) {
+            cout << "now : " << now->num_value << endl;
+            cout << "str : " << str << endl;}
             else
-            cout << "now's before : NULL" << endl;
-            /*
+            cout << "now : NULL" << endl;
+
+            
+            
             if (now->num_value == str) {
+                cout << "Well done" << endl;
+                
                 now = now->before;
-                now->nextarrayindex++;
+                asdf = now->num_value;
+                // ????
+                /* 컴파일에러 왜뜸
+                now->nextarrayindex++; 돌겠네 */
             } else {
                 cout << "Error!" << endl;
-            } */
+            }
+
+            
 
             cout << endl;
         } else { // 여는 태그일 시
@@ -149,12 +158,12 @@ int main() { // main
             
             cout << "Vector pushed: " << endl;
 
-            cout << "v" << v.size() << " number: " << now->num_value << endl << endl;
-            /*
-            cout << "v" << v.size() << " name: " << v[count - 1].name[count - 1] << endl;
-            cout << " value: " << v[count - 1].value[count - 1] << endl;
+            cout << "v" << v.size() << " number: " << now->num_value << endl;
+            cout << "v" << v.size() << " name: " << v[count - 1].name[0] << endl;
+            cout << "v value: " << v[count - 1].value[0] << endl;
+            cout << "count : " << count << endl;
+            cout << endl;
             
-            */
             //value 처리에서 runtime error
             closetag = false;
         }
